56d999eea2f36e3cab0774a0b30b53c5078530a1
Lawrence Hsu <lawrence>
2024-10-31 16:32:51 +0800

    Auto-generated commit

--- src/app/models/video_item.ts
+++
@@ -1,4 +0,0 @@
-export interface Video {
-    title: string;
-    video_id: string;
-}
--- src/app/pages/roadmap/roadmap.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { RoadmapComponent } from './roadmap.component';
-
-export const ROADMAP_ROUTES: Routes = [
-  { path: '', component: RoadmapComponent },
-];
